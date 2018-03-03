f978f9dc1f3b55c6d79c4d3c4505da5fbef6c74e
Amajid Sinar <amajidsinar@gmail.com>
2018-03-03 15:40:11 +0700

    a little housekeeping

--- knn-oop.py
+++ knn-oop.py
@@ -14,82 +14,90 @@ Created on Tue Feb 20 23:30:12 2018
 @author: amajidsinar
 """
 
-class Knn:
-    import numpy as np
-    from scipy import stats
-    import matplotlib.pyplot as plt
-    import random
-    plt.style.use("seaborn-white")
-    
-    def split(self,data_known,label_known,training_percentage):
-        import random
-        #data_set and label is static
-        data_set = data_known
-        label = label_known
-        #take percentage*len(data)
-        index = random.sample(range(len(data_known)),int(training_percentage*len(data_known)))
-        data_known = data_set[index]
-        label_known =  label[index]
-        data_unknown = np.delete(data_set, index, axis=0)
-        label_unknown = np.delete(label, index, axis=0)
-        return (data_known,label_known,data_unknown,label_unknown)
-    
-    def plot(self,title,xlabel,ylabel):
-        category = np.unique(label_known)
-        for i in category:
-            upper = np.max(np.where(label_known == i))
-            lower = np.min(np.where(label_known == i))
-            plt.scatter(data_known[lower:upper,0],data_known[lower:upper,1],label=i)
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.scatter(data_unknown[:,0],data_unknown[:,1], label='?')
-        plt.legend()
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-white")
+
+class Model:
+    def evaluate(self):
+        print(self.predict())
+        print(self.label_unknown)
+        return np.mean(self.predict() == self.label_unknown)
+
+class Knn(Model):
+   
+    def __init__(self, k, dist='euc'):
+        avDist = ['euc', 'man']
+        if dist not in avDist:
+            pass
+        self.k = k
+        self.dist = dist
         
-    def euclidean_distance(self,data_known,data_unknown):
-        diff = data_known - data_unknown.reshape((data_unknown.shape[0],1,data_unknown.shape[1]))
-        distance = (diff**2).sum(2)
-        distance = distance.reshape(data_unknown.shape[0],1,data_known.shape[0])
-        return distance
     
-    def predict(self,data_known,data_unknown,label_known,k):
+    def fit(self,data_known,label_known,data_unknown,label_unknown):
+        self.data_known = data_known
+        self.label_known = label_known
+        self.data_unknown = data_unknown
+        self.label_unknown = label_unknown
+        
+    def L1_distance(self):
+        diff = self.data_known - self.data_unknown.reshape((self.data_unknown.shape[0],1,self.data_unknown.shape[1]))
+        return (diff**2).sum(2)
+    
+    def manhattan(self):
+        diff = self.data_known - self.data_unknown.reshape((self.data_unknown.shape[0],1,self.data_unknown.shape[1]))
+        return np.abs(diff).sum(2)
+        
+        
+    def L2_distance(self):
+        diff = self.data_known - self.data_unknown.reshape((self.data_unknown.shape[0],1,self.data_unknown.shape[1]))
+        return np.sqrt((diff**2).sum(2))
+    
+    
+    def predict(self):
         #sort label
-        dist_index = np.argsort(self.euclidean_distance(data_known,data_unknown))
-        label = label_known[dist_index]
+        if self.dist == 'euc':
+            dist_index = np.argsort(self.L2_distance())
+        else:
+            dist_index = np.argsort(self.manhattan())
+        
+        label = self.label_known[dist_index]
         #only pick until kth index
-        label = label[:,:,:k]
+        label = label[:,:self.k]
         #return the mode
-        label_unknown = []
-        for i in range(len(data_unknown)):
+        label_predict = []
+        for i in range(self.data_unknown.shape[0]):
             values,counts =  np.unique(label[i], return_counts=True)
             ind = np.argmax(counts)
-            label_unknown.append(values[ind])
-        return label_unknown
-
+            label_predict.append(values[ind])
+        return label_predict
     
         
+import random
+def split(data_known,label_known,training_percentage):
+    #data_set and label is static
+    data_set = data_known
+    label = label_known
+    #take percentage*len(data)
+    index = random.sample(range(len(data_known)),int(training_percentage*len(data_known)))
+    data_known = data_set[index]
+    label_known =  label[index]
+    data_unknown = np.delete(data_set, index, axis=0)
+    label_unknown = np.delete(label, index, axis=0)
+    return (data_known,label_known,data_unknown,label_unknown)
+
 #load iris
-import numpy as np
 from sklearn import datasets
 iris = datasets.load_iris()
-
-#data_known = iris.data[:,:2]
-#label_known = iris.target
-#data_unknown = np.array([[5.7,3.3],[5.6,3.4]])
 
 data_known = iris.data[:,:2]
 label_known = iris.target
 
-knn = Knn()
-data_known, label_known, data_unknown, label_unknown = knn_1.split(data_known,label_known,0.4)
-#set input
-#first = trainingAndTest(iris.data, iris.target, 0.4)
-#data_known,label_known,data_unknown,label_unknown = first()
-predict1 = knn.predict(data_known, data_unknown, label_known,1)
-predict2 = knn.predict(data_known, data_unknown, label_known,2)
-predict3 = knn.predict(data_known, data_unknown, label_known,3)
-predict4 = knn.predict(data_known, data_unknown, label_known,4)
-predict5 = knn.predict(data_known, data_unknown, label_known,5)
+data_known,label_known,data_unknown,label_unknown = split(data_known,label_known,0.9)
+
+knn1 = Knn(5,dist='eu')
+#knn1.fit(data_known,label_known,data_unknown,label_unknown)
+#performance = knn1.evaluate()
 
 
 
--- knn.py
+++ knn.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('seaborn-white')
 
-
 iris = datasets.load_iris()
 
 dataset = iris.data
@@ -26,36 +25,32 @@ label_known = iris.target
 # the same color
 category = np.unique(label_known)
 for i in category:
-    upper = np.max(np.where(label_known == i))  
-    lower = np.min(np.where(label_known == i))
-    plt.scatter(data_known[lower:upper,0],data_known[lower:upper,1],label=i)
+    plt.scatter(data_known[label_known==i][:,0],data_known[label_known==i][:,1],label=i)
 
 # Unknown class of a data
-data_unknown = np.array([[5.7,3.3],[5.6,3.4]])
+data_unknown = np.array([[5.7,3.3],[5.6,3.4],[6.4,3],[8.2,2.2]])
 plt.scatter(data_unknown[:,0],data_unknown[:,1], label='?')
 plt.legend()
 #-------------
 # Euclidean Distance
 diff = data_known - data_unknown.reshape(data_unknown.shape[0],1,data_unknown.shape[1])
 distance = (diff**2).sum(2)
-distance = distance.reshape(data_unknown.shape[0],1,data_known.shape[0])
-
 
 #return sorted index of distance
 dist_index = np.argsort(distance)
 label = label_known[dist_index]
 
-k = 5
-
+#for k in [1,2,3,4,5,6,7,8,9,10]:
 #keep the rank
-label = label[:,:,:k]
+k = 10
+label = label[:,:k]
 
-label_unknown = []    
-for i in range(len(data_unknown)):
+label_predict = []
+for i in range(data_unknown.shape[0]):
     values,counts =  np.unique(label[i], return_counts=True)
     ind = np.argmax(counts)
-    label_unknown.append(values[ind])
-
+    label_predict.append(values[ind])
+    
 
 
 
